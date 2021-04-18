fde91452768eb5791c852b57520c7f348a7f3a28
Dhananjay <guptadhananjay10@gmail.com>
2021-04-18 14:39:31 +0530

    Add All New File Project

---
+++ app/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.http.response import HttpResponse
+from django.urls import path
+from app import views
+
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('login', views.login, name='login'),
+    path('signup/', views.signup),
+    path('add-todo/', views.add_todo),    
+    path('delete-todo/<int:id>' , views.delete_todo ), 
+    path('change-status/<int:id>/<str:status>' , views.change_todo ), 
+    path('logout/' , views.signout ), 
+]
